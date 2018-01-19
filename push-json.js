91b8a532430f368aaae690e1c1416690ccd09a42
Chris Whong <chris@whong.org>
2018-01-19 14:48:19 -0500

    add post-test script

---
+++ push-json.js
@@ -0,0 +1,49 @@
+var SlackWebhook = require('slack-webhook');
+var AWS = require('aws-sdk');
+
+require('dotenv').config();
+
+var spacesEndpoint = new AWS.Endpoint('nyc3.digitaloceanspaces.com');
+var s3 = new AWS.S3({
+    endpoint: spacesEndpoint,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+});
+
+var slack = new SlackWebhook(process.env.SLACK_WEBHOOK_URL, {
+  defaults: {
+    username: 'GeoSearch Import Bot',
+    channel: '#labs-geocoder-api',
+    icon_emoji: ':robot_face:'
+  }
+});
+
+var normalizedPath = require('path').join(__dirname, 'failures');
+
+var failures = [];
+
+require('fs').readdirSync(normalizedPath)
+  .forEach(function(file) {
+    var failure = require('./failures/' + file);
+    failures.push(failure);
+  });
+
+// fs.emptyDir('./tmp');
+// fs.writeFile('./tmp/failures.json', JSON.stringify(failures));
+
+var params = {
+    Body: JSON.stringify(failures),
+    Bucket: 'planninglabs',
+    Key: 'geosearch-acceptance-tests/failures.json',
+    ContentType: 'application/json',
+    ACL: 'public-read',
+};
+
+s3.putObject(params, function(err) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      var url = 'https://planninglabs.nyc3.digitaloceanspaces.com/geosearch-acceptance-tests/failures.json';
+      slack.send(`GeoSearch Tests Complete! <${url}|View JSON>`);
+    }
+});
